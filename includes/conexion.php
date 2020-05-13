074adbc0faf5bee367a593e84c6dcd1774b9cfe6
Adrian-ICO <adrian.ga_2009@hotmail.com>
2020-05-13 02:05:39 -0500

    restart :v

--- includes/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-$con = mysqli_connect("localhost","user","pass","base de datos");
-// checamos la conexion
-if (mysqli_connect_errno())
-{
-echo "Error al conectarse con MYSQL: " . mysqli_connect_error();
-}
-?>
