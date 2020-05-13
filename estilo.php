61d316d49220e359d0eb4ddaf5cf7f261c1b0d48
Adrian-ICO <adrian.ga_2009@hotmail.com>
2020-05-13 16:04:18 -0500

    center

--- estilo.php
+++ estilo.php
@@ -101,8 +101,6 @@ $u=rand(5,0);
     display:flex;
     justify-content:center;
     text-align:center;
-    line-height: 110%;
-    margin: 2.3733333333rem 0 1.424rem 0;
     color: white;
 }
 .img1 {
