7966187288e134e7e2d8a4b49b4bf3fb34d679d4
Adrian-ICO <adrian.ga_2009@hotmail.com>
2020-05-13 16:18:31 -0500

    top

--- estilo.php
+++ estilo.php
@@ -92,8 +92,9 @@ $u=rand(5,0);
     display:flex;
     justify-content:flex-start;
     line-height: 110%;
-    margin-left: 20%;
-    margin: 2.3733333333rem 0 1.424rem 0;
+    margin-left: 1%;
+    margin-top: 1%;
+ 
 }
 .shield{
     display:flex;
