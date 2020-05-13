33b6e9cf916cfc26a47a35ba1fb898c768036afb
Adrian-ICO <adrian.ga_2009@hotmail.com>
2020-05-12 21:27:59 -0500

    styles

--- index/index.php
+++ index/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" type="text/css" href="../estilo.php">
+    <link rel="stylesheet" type="text/css" href="VAGA/styles/estilo.php">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
--- styles/estilo.php
+++ styles/estilo.php
@@ -1,4 +1,7 @@
 h2{
+    color: black;
+    font-size: 2em;
+    font-weight: lighter;
     font-size: 3.56rem;
     line-height: 110%;
     margin: 2.3733333333rem 0 1.424rem 0;
