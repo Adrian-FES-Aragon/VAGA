5faaa8c1e30caa8cb551aaaeee77daa259a3f70b
MauricioMiramontes <mmiramontesr@gmail.com>
2020-06-18 18:04:50 -0500

    Se agrega base de datos

---
+++ DB/Data.sql
@@ -0,0 +1,5 @@
+USE `php_test`;
+
+INSERT INTO `usuarios` (`id`,`user`, `nombre`, `correo`, `password`,`date`,`perm`) VALUES
+(1, 'root','Gabriel Ore Durán', 'allright@gmail.com', '0000','1000-01-10 00:00:00',1),
+(2, 'admin','Alucard Van Hellsing', 'inhisworld@gmail.com', '1111','1998-01-10 00:00:00',2);
--- DB/db.sql
+++ DB/db.sql
@@ -4,22 +4,20 @@ CREATE SCHEMA IF NOT EXISTS `php_test` DEFAULT CHARACTER SET utf8 collate utf8_s
 USE `php_test`;
 
 CREATE TABLE `usuarios` (
-`id` int(99) NOT NULL,
-`user` varchar(30) NOT NULL,
-`nombre` varchar(300) NOT NULL,
-`correo` varchar(300) NOT NULL,
-`password` varchar(450) NOT NULL,
-`date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-`perm` int(11) NOT NULL DEFAULT '3'
+    `id` int UNSIGNED NOT NULL PRIMARY KEY auto_increment, 
+    `user` varchar(30) NOT NULL,
+    `nombre` varchar(300) NOT NULL,
+    `correo` varchar(300) NOT NULL,
+    `password` varchar(450) NOT NULL,
+    `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `perm` int(11) NOT NULL DEFAULT '3'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `usuarios` (`id`,`user`, `nombre`, `correo`, `password`,`date`,`perm`) VALUES
-(1, 'root','Gabriel Ore Durán', 'allright@gmail.com', '0000','0000-0-00 00:00:00',1),
-(2, 'admin','Alucard Van Hellsing', 'inhisworld@gmail.com', '1111','1998-1-10 00:00:00',2);
-
-ALTER TABLE `usuarios`
-    ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `usuarios`
-MODIFY `id` int(99) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-COMMIT;
+CREATE TABLE `bienes` (
+  `TieneDueño` boolean NOT NULL,
+  `EslapTop` boolean NOT NULL, 
+  `id` int UNSIGNED NOT NULL PRIMARY KEY auto_increment,
+  `id_dueño` int UNSIGNED NOT NULL,
+  KEY `id_dueño` (`id_dueño`),
+  CONSTRAINT `ID_DUEÑO` FOREIGN KEY (`id_dueño`) REFERENCES `usuarios` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
